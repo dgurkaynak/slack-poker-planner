88ed3caa3b408141abe5eb8f499c0959ed872574
Deniz Gurkaynak <dgurkaynak@gmail.com>
2019-10-01 08:21:39 +0300

    :sparkles: Enable slack app suggestion

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ module.exports = (request, reply) => {
     return reply.view('index', {
         SLACK_CLIENT_ID: process.env.SLACK_CLIENT_ID,
         SLACK_SCOPE: process.env.SLACK_SCOPE,
+        SLACK_APP_ID: process.env.SLACK_APP_ID,
         COUNTLY_URL: process.env.COUNTLY_URL,
         COUNTLY_APP_KEY: process.env.COUNTLY_APP_KEY
     });
--- src/routes/privacy.js
+++ src/routes/privacy.js
@@ -1,5 +1,6 @@
 module.exports = (request, reply) => {
     return reply.view('privacy', {
+        SLACK_APP_ID: process.env.SLACK_APP_ID,
         COUNTLY_URL: process.env.COUNTLY_URL,
         COUNTLY_APP_KEY: process.env.COUNTLY_APP_KEY
     });
