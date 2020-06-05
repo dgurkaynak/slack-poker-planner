6a2b54d21af683c07b1d70ecebe49b67a336cd90
Deniz Gurkaynak <dgurkaynak@gmail.com>
2020-06-05 23:16:12 +0300

    refactor(pp-command): depracate `/pp config` & update `/pp help`

--- src/routes/pp-command.ts
+++ src/routes/pp-command.ts
@@ -215,177 +215,13 @@ export class PPCommandRoute {
    * `/pp config ...`
    */
   static async configure(cmd: ISlackCommandRequestBody, res: express.Response) {
-    const [findErr, team] = await to(TeamStore.findById(cmd.team_id));
-
-    if (findErr) {
-      const errorId = generateId();
-      logger.error(
-        `(${errorId}) Could not created topic, could not get the team from db`,
-        cmd,
-        findErr
-      );
-      return res.json({
-        text:
-          `Internal server error, please try again later\n` +
-          `SC_TEAM_GET_FAIL (${errorId})\n\n` +
-          `If this problem is persistent, you can open an issue on <${process.env.ISSUES_LINK}>`,
-        response_type: 'ephemeral',
-        replace_original: false,
-      });
-    }
-
-    if (!team) {
-      logger.error(`Could not created topic, team could not be found`, cmd);
-      return res.json({
-        text:
-          `Your slack team "${cmd.team_domain}" could not be found, please add Poker Planner to ` +
-          `your Slack team again on <${process.env.APP_INSTALL_LINK}>`,
-        response_type: 'ephemeral',
-        replace_original: false,
-      });
-    }
-
-    // Get the text after `/pp config` part
-    const text = cmd.text.replace('config ', '');
-
-    if (!text) {
-      return res.json({
-        text: `You have to enter at least 2 poker values seperated by space`,
-        response_type: 'ephemeral',
-        replace_original: false,
-      });
-    }
-
-    // Reset the custom points
-    if (text == 'reset') {
-      return PPCommandRoute.resetCustomPoints(cmd, res);
-    }
-
-    return PPCommandRoute.updateCustomPoints(cmd, res);
-  }
-
-  /**
-   * `/pp config reset`
-   */
-  static async resetCustomPoints(
-    cmd: ISlackCommandRequestBody,
-    res: express.Response
-  ) {
-    const [updateErr] = await to(TeamStore.updateCustomPoints(cmd.team_id, ''));
-
-    if (updateErr) {
-      const errorId = generateId();
-      logger.error(
-        `(${errorId}) Could not reset custom points, db error`,
-        cmd,
-        updateErr
-      );
-      return res.json({
-        text:
-          `Internal server error, please try again later\n` +
-          `SC_RESET_FAIL (${errorId})\n\n` +
-          `If this problem is persistent, you can open an issue on <${process.env.ISSUES_LINK}>`,
-        response_type: 'ephemeral',
-        replace_original: false,
-      });
-    }
-
-    logger.info(
-      `[${cmd.team_domain}(${cmd.team_id})] ${cmd.user_name}(${cmd.user_id}) reset custom points`
-    );
-
-    if (process.env.COUNTLY_APP_KEY) {
-      Countly.add_event({
-        key: 'reset_custom_points',
-        count: 1,
-        segmentation: {},
-      });
-    }
-
-    return res.send(
-      `Poker planner's configuration is successfully reset to default values`
-    );
-  }
-
-  /**
-   * `/pp config value1 value2 value3 ...`
-   */
-  static async updateCustomPoints(
-    cmd: ISlackCommandRequestBody,
-    res: express.Response
-  ) {
-    // Parse the custom points
-    const text = cmd.text.replace('config ', '');
-    const customPointsArr = text.match(/\S+/g) || [];
-    const customPoints = customPointsArr.join(' ');
-
-    if (customPointsArr.length < 2) {
-      return res.json({
-        text: `You have to enter at least 2 poker values seperated by space`,
-        response_type: 'ephemeral',
-        replace_original: false,
-      });
-    }
-
-    if (customPointsArr.length > 25) {
-      return res.json({
-        text: `Maximum 25 values are supported`,
-        response_type: 'ephemeral',
-        replace_original: false,
-      });
-    }
-
-    for (let customPoint of customPointsArr) {
-      if (customPoint.length > 20) {
-        return res.json({
-          text: `Poker values must be under 20 characters`,
-          response_type: 'ephemeral',
-          replace_original: false,
-        });
-      }
-    }
-
-    // Update the custom points
-    const [updateErr] = await to(
-      TeamStore.updateCustomPoints(cmd.team_id, customPoints)
-    );
-
-    if (updateErr) {
-      const errorId = generateId();
-      logger.error(
-        `(${errorId}) Could not set custom points, db error`,
-        cmd,
-        updateErr
-      );
-      return res.json({
-        text:
-          `Internal server error, please try again later\n` +
-          `SC_SET_FAIL (${errorId})\n\n` +
-          `If this problem is persistent, you can open an issue on <${process.env.ISSUES_LINK}>`,
-        response_type: 'ephemeral',
-        replace_original: false,
-      });
-    }
-
-    logger.info(
-      `[${cmd.team_domain}(${cmd.team_id})] ${cmd.user_name}(${cmd.user_id}) configured custom points: ${customPoints}`
-    );
-
-    if (process.env.COUNTLY_APP_KEY) {
-      Countly.add_event({
-        key: 'set_custom_points',
-        count: 1,
-        segmentation: {
-          values: customPointsArr.length,
-        },
-      });
-    }
-
-    return res.send(
-      `Poker planner will use ${
-        customPointsArr.length
-      } poker values from now on: ${customPointsArr.join(', ')}`
-    );
+    return res.json({
+      text:
+        'This command is depracated. The session settings (points, participants, ...) ' +
+        'are now persisted automatically for each channel/conversation.',
+      response_type: 'ephemeral',
+      replace_original: false,
+    });
   }
 
   /**
@@ -400,39 +236,15 @@ export class PPCommandRoute {
         {
           color: '#3AA3E3',
           text:
+            '`/pp`\n' +
+            'Opens a dialog to start a new poker planning session.',
+        },
+        {
+          color: '#3AA3E3',
+          text:
             '`/pp some topic text`\n' +
-            'Starts a poker planning session on specified, ' +
-            'topic, or simply anything you typed after `/pp`. ' +
-            'This command will automatically find active (online ' +
-            'and not-away) users in the current channel/group and ' +
-            'add them as participants to poker planning session.',
-        },
-        {
-          color: '#3AA3E3',
-          text:
-            '`/pp some topic text @user1 @user2`\n' +
-            'This command works exactly like above, however ' +
-            'specifically mentioned users will be added to the ' +
-            'session, even if they are offline. Mentions must ' +
-            'come after the topic text.',
-        },
-        {
-          color: '#3AA3E3',
-          text:
-            '`/pp config value1 value2 ...`\n' +
-            'This command lets you customize poker values just ' +
-            'for your team. After this, all the voting sessions ' +
-            'will have just these options, until you configure ' +
-            'Poker Planner again with `/pp config` command.\n\n' +
-            'At least 2 values must be provided, seperated with ' +
-            'space character. Each value cannot be more than 20 ' +
-            'characters, and maximum 25 poker values are supported.',
-        },
-        {
-          color: '#3AA3E3',
-          text:
-            '`/pp config reset`\n' +
-            'This command resets the poker values back to default settings.',
+            'Opens the same dialog, however title input is automatically ' +
+            'filled with the value you provided.',
         },
       ],
     });
