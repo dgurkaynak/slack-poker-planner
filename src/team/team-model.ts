b0b85b19e7f273119e05b348c171e35a8f1c3936
Deniz Gurkaynak <dgurkaynak@gmail.com>
2020-06-07 21:03:47 +0300

    feat(team-modal): instrumentaion

--- src/team/team-model.ts
+++ src/team/team-model.ts
@@ -1,4 +1,5 @@
 import * as sqlite from '../lib/sqlite';
+import { Trace, getSpan } from '../lib/trace-decorator';
 
 export interface ITeam {
   id: string;
@@ -23,54 +24,94 @@ export interface IChannelSetting {
 }
 
 export class TeamStore {
+  @Trace()
   static async findById(id: string): Promise<ITeam> {
+    const span = getSpan();
+    span?.setAttribute('id', id);
     const db = sqlite.getSingleton();
     return db.get('SELECT * FROM team WHERE id = ?', id);
   }
 
-  static async upsert(
-    data: Pick<ITeam, 'id' | 'name' | 'access_token' | 'scope' | 'user_id'>
-  ) {
+  @Trace()
+  static async create({
+    id,
+    name,
+    access_token,
+    scope,
+    user_id,
+  }: Pick<ITeam, 'id' | 'name' | 'access_token' | 'scope' | 'user_id'>) {
+    const span = getSpan();
+    span?.setAttributes({ id, name, scope, user_id });
     const db = sqlite.getSingleton();
-    const team = await TeamStore.findById(data.id);
-    if (!team) {
-      await db.run(
-        `INSERT INTO
+    await db.run(
+      `INSERT INTO
           team (id, name, access_token, scope, user_id)
         VALUES
           ($id, $name, $access_token, $scope, $user_id)`,
-        {
-          $id: data.id,
-          $name: data.name,
-          $access_token: data.access_token,
-          $scope: data.scope,
-          $user_id: data.user_id,
-        }
-      );
-    } else {
-      await db.run(
-        `UPDATE
-          team
-        SET
-          name = $name,
-          access_token = $access_token,
-          scope = $scope,
-          user_id = $user_id
-        WHERE
-          id = $id`,
-        {
-          $id: data.id,
-          $name: data.name,
-          $access_token: data.access_token,
-          $scope: data.scope,
-          $user_id: data.user_id,
-        }
-      );
-    }
-    return TeamStore.findById(data.id);
+      {
+        $id: id,
+        $name: name,
+        $access_token: access_token,
+        $scope: scope,
+        $user_id: user_id,
+      }
+    );
   }
 
+  @Trace()
+  static async update({
+    id,
+    name,
+    access_token,
+    scope,
+    user_id,
+  }: Pick<ITeam, 'id' | 'name' | 'access_token' | 'scope' | 'user_id'>) {
+    const span = getSpan();
+    span?.setAttributes({ id, name, scope, user_id });
+    const db = sqlite.getSingleton();
+    await db.run(
+      `UPDATE
+        team
+      SET
+        name = $name,
+        access_token = $access_token,
+        scope = $scope,
+        user_id = $user_id
+      WHERE
+        id = $id`,
+      {
+        $id: id,
+        $name: name,
+        $access_token: access_token,
+        $scope: scope,
+        $user_id: user_id,
+      }
+    );
+  }
+
+  @Trace()
+  static async upsert({
+    id,
+    name,
+    access_token,
+    scope,
+    user_id,
+  }: Pick<ITeam, 'id' | 'name' | 'access_token' | 'scope' | 'user_id'>) {
+    const span = getSpan();
+    span?.setAttributes({ id, name, scope, user_id });
+    const team = await TeamStore.findById(id);
+    if (!team) {
+      await TeamStore.create({ id, name, access_token, scope, user_id });
+    } else {
+      await TeamStore.update({ id, name, access_token, scope, user_id });
+    }
+    return TeamStore.findById(id);
+  }
+
+  @Trace()
   static async fetchSettings(teamId: string, channelId: string) {
+    const span = getSpan();
+    span?.setAttributes({ teamId, channelId });
     const db = sqlite.getSingleton();
     const settingRows = await db.all(
       `SELECT
@@ -95,34 +136,51 @@ export class TeamStore {
     return rv;
   }
 
+  @Trace()
   static async upsertSettings(
     teamId: string,
     channelId: string,
     settings: { [key: string]: string }
   ) {
-    const db = sqlite.getSingleton();
+    const tasks = Object.keys(settings).map((settingKey) =>
+      TeamStore.upsertSetting(
+        teamId,
+        channelId,
+        settingKey,
+        settings[settingKey]
+      )
+    );
+    await Promise.all(tasks);
+  }
 
-    for (const settingKey of Object.keys(settings)) {
-      const settingValue = settings[settingKey];
-      await db.run(
-        `INSERT INTO
-          channel_settings (team_id, channel_id, setting_key, setting_value)
-        VALUES (
-          $teamId,
-          $channelId,
-          $settingKey,
-          $settingValue
-        )
-        ON CONFLICT(team_id, channel_id, setting_key)
-        DO UPDATE SET setting_value = $settingValue;`,
-        {
-          $teamId: teamId,
-          $channelId: channelId,
-          $settingKey: settingKey,
-          $settingValue: settingValue,
-        }
-      );
-    }
+  @Trace()
+  static async upsertSetting(
+    teamId: string,
+    channelId: string,
+    key: string,
+    value: string
+  ) {
+    const span = getSpan();
+    span?.setAttributes({ teamId, channelId, key, value });
+    const db = sqlite.getSingleton();
+    await db.run(
+      `INSERT INTO
+        channel_settings (team_id, channel_id, setting_key, setting_value)
+      VALUES (
+        $teamId,
+        $channelId,
+        $settingKey,
+        $settingValue
+      )
+      ON CONFLICT(team_id, channel_id, setting_key)
+      DO UPDATE SET setting_value = $settingValue;`,
+      {
+        $teamId: teamId,
+        $channelId: channelId,
+        $settingKey: key,
+        $settingValue: value,
+      }
+    );
   }
 
   static async updateCustomPoints(teamId: string, customPoints: string) {
