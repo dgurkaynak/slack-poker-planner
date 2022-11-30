ed3178621c49f076f274f78bc8101e4a02b454e7
hendrik.nielaender@taxdoo.com <hendrik.nielaender@taxdoo.com>
2022-11-30 14:49:36 +0100

    fix: app build & remove node-fetch

--- esbuild.js
+++ esbuild.js
@@ -4,7 +4,7 @@ require('esbuild').build({
     entryPoints: ['./src/app.ts'],
     bundle: true,
     platform: 'node',
-    target: 'node16.18',
-    external: ['aws-sdk', 'mock-aws-s3', 'nock', 're2'],
+    target: 'node18.12',
+    external: ['aws-sdk', 'mock-aws-s3', 'nock', 're2', 'sqlite3'],
     outfile: './dist/app.js',
   }).catch(() => process.exit(1))
